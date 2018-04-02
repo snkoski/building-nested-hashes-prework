55752f1087dab53ecdf8a58ee71bcdb52fb7b82f
Shawn Koski <shawnnkoski@gmail.com>
2018-04-02 10:31:34 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,9 +2,17 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch => {}
+      :matriaarch => {}
+      :hero => {}
+      :hero_friends =>{}
       }, 
     :capulet => {
       #your key/value pairs here!
+      :patriarch => {}
+      :matriaarch => {}
+      :hero => {}
+      :hero_friends =>{}
       }}
 
   
