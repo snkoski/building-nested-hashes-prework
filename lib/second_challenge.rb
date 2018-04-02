523431fc38e08172b5d716ce926cbe2a0aeb45a0
Shawn Koski <shawnnkoski@gmail.com>
2018-04-02 10:35:26 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,14 +3,14 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       :patriarch => {},
-      :matriaarch => {},
+      :matriarch => {},
       :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
-      :matriaarch => {},
+      :matriarch => {},
       :heroine => {},
       :heroine_friends => []
       }}
